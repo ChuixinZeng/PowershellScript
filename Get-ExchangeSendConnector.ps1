6d82de6d3de79fe7a3fa2448a7bd9e373eb2f017
zengchuixin <zengchuixin@126.com>
2018-06-05 10:22:35 +0800

    Exchange | 增加exchange运维脚本

---
+++ Get-ExchangeSendConnector.ps1
@@ -0,0 +1,2 @@
+﻿Get-SendConnector | select AddressSpaces,Name,MaxMessageSize,Enabled,IsSmtpConnector,Port,WhenCreated,WhenChang
+ed | Export-Csv C:\connector.csv -Encoding default -NoTypeInformation
