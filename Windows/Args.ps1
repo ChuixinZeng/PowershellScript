54c5ecaac9f854b5e5f1608afe93ce94a052ea32
zengchuixin <zengchuixin@360.cn>
2019-04-23 15:15:02 +0800

    Windows | 增加注释符号

--- Windows/Args.ps1
+++ Windows/Args.ps1
@@ -7,10 +7,12 @@ Set-Alias tc Test-Conn
 tc localhost
 ½á¹û
 
+'''
 tc localhost
 
-Source        Destination     IPV4Address      IPV6Address                     
-------        -----------     -----------      -----------                     
-5CD60537FF    localhost       127.0.0.1        ::1                             
-5CD60537FF    localhost       127.0.0.1        ::1                             
-5CD60537FF    localhost       127.0.0.1        ::1      
+Source        Destination     IPV4Address      IPV6Address
+------        -----------     -----------      -----------
+5CD60537FF    localhost       127.0.0.1        ::1
+5CD60537FF    localhost       127.0.0.1        ::1
+5CD60537FF    localhost       127.0.0.1        ::1
+'''
