6efc1eebb88655ecb5df17af1e6647b77c2421af
zengchuixin <zengchuixin@360.cn>
2019-04-23 14:24:43 +0800

    Windows | 增加管理进程的脚本

---
+++ Windows/Get-Process.ps1
@@ -0,0 +1,8 @@
+﻿Get-Process powershell -ComputerName zengchuixin-pc,zengchuixin-pc,zengchuixin-pc |
+    Format-Table -Property Handles,
+        @{Label="NPM(K)";Expression={[int]($_.NPM/1024)}},
+        @{Label="PM(K)";Expression={[int]($_.PM/1024)}},
+        @{Label="WS(K)";Expression={[int]($_.WS/1024)}},
+        @{Label="VM(M)";Expression={[int]($_.VM/1MB)}},
+        @{Label="CPU(s)";Expression={if ($_.CPU -ne $()){$_.CPU.ToString("N")}}},
+        Id, ProcessName, MachineName -auto
