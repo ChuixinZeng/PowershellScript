3a7e71b14c90b40bceb10d527b6701f11dfc3601
zengchuixin <zengchuixin@126.com>
2019-06-17 01:24:41 +0800

    windows | 停止计算机（单台或多台）

---
+++ Windows/Stop-Computer.ps1
@@ -0,0 +1,17 @@
+﻿# Try without doing anything bad
+
+Stop-Computer -WhatIf
+
+# Stop the local computer
+
+Stop-Computer
+
+# Try without doing anything bad on multiple systems
+
+Stop-computer -ComputerName ‘computer01′,’computer02′,’computer03’ -whatif
+
+# Stop multiple systems
+
+Stop-computer -ComputerName ‘computer01′,’computer02′,’computer03’
+
+# https://devblogs.microsoft.com/scripting/powertip-turn-off-the-power-to-your-computer-with-powershell/
