246e1e1afa218035cd52f704e114c853e99c0472
zengchuixin <zengchuixin@126.com>
2019-06-17 01:23:57 +0800

    windows | 检查证书签名文件的证书信息

---
+++ Windows/Get-CertSignStatus.ps1
@@ -0,0 +1,22 @@
+﻿Function Expand-AuthenticodeSignature($AuthenticodeSignature)
+{
+$AuthenticodeSignature | Select-object -Property Path,
+Status,StatusMessage,SignatureType,
+@{Name=’SubjectName’;Expression={($_.SignerCertificate.Subject)}},
+@{Name='SubjectIssuer';Expression={($_.SignerCertificate.Issuer)}},
+@{Name=’SubjectSerialNumber’;Expression={($_.SignerCertificate.SerialNumber)}},
+@{Name='SubjectNotBefore';Expression={($_.SignerCertificate.NotBefore)}},
+@{Name=’SubjectNotAfter’;Expression={($_.SignerCertificate.NotAfter)}},
+@{Name='SubjectThumbprint';Expression={($_.SignerCertificate.ThumbPrint)}},
+@{Name=’TimeStamperName’;Expression={($_.SignerCertificate.Subject)}},
+@{Name='TimeStamperIssuer';Expression={($_.SignerCertificate.Issuer)}},
+@{Name=’TimeStamperSerialNumber’;Expression={($_.SignerCertificate.SerialNumber)}},
+@{Name='TimeStamperNotBefore';Expression={($_.SignerCertificate.NotBefore)}},
+@{Name=’TimeStamperNotAfter’;Expression={($_.SignerCertificate.NotAfter)}}, `
+@{Name=’TimeStamperThumbprint’;Expression={($_.SignerCertificate.ThumbPrint)}}}
+
+$List=Get-ChildItem C:\Windows\*.exe | Get-AuthenticodeSignature
+
+Expand-AuthenticodeSignature -AuthenticodeSignature $List | Export-Csv C:\report\working.csv
+
+# https://devblogs.microsoft.com/scripting/reporting-on-digitally-signed-files-with-powershell/
