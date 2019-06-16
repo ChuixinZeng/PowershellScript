560ef7c8b8910bc22520d5fc8a6022fa3748f8b5
zengchuixin <zengchuixin@126.com>
2019-06-17 01:24:15 +0800

    Windows | 生成随机数

---
+++ Windows/Get-Random.ps1
@@ -0,0 +1,5 @@
+﻿$NameList=’John’,’Charlotte’,’Sean’,’Colleen’,’Namoli’,’Maura’,’Neula’
+
+Get-Random -InputObject $NameList
+
+# https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-pick-a-random-name-from-a-list/
