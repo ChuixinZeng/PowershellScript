771f4916e3f5719108845dfb3a663f5e27d6c936
zengchuixin <zengchuixin@126.com>
2019-08-07 18:07:18 +0800

    Windows | 读取WAV文件

---
+++ Windows/Play-WAVFile.ps1
@@ -0,0 +1,7 @@
+﻿$PlayWav=New-Object System.Media.SoundPlayer
+
+$PlayWav.SoundLocation=’C:\Foo\Soundfile.wav’
+
+$PlayWav.playsync()
+
+# https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-play-wav-files/
