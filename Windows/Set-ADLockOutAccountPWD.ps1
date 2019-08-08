17c3888895c0ffb30df8a36f2fc1d46647408875
zengchuixin <zengchuixin@126.com>
2019-08-08 15:12:33 +0800

    windows | 开启锁定的AD账户并修改密码

---
+++ Windows/Set-ADLockOutAccountPWD.ps1
@@ -0,0 +1,10 @@
+﻿$Password=’T00muchCh33r’
+$SecurePassword=ConvertTo-SecureString -asplaintext -force $Password
+$Locked=Search-ADAccount-LockedOut
+Foreach ( $User in $Locked )
+{
+Set-ADAccountPassword -Identity:$User.DistinguishedName -NewPassword:$SecurePassword -Reset:$true -Server:"ContosoDC01.Contoso.Com"
+Set-ADUser -ChangePasswordAtLogon:$true -Identity:$User.DistinguishedName -Server:" ContosoDC01.Contoso.Com "
+}
+
+# https://devblogs.microsoft.com/scripting/curly-blue-and-the-meaning-of-scripting-part-5/
