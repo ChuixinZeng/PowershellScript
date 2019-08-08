d86dd810f968b458b0f4ba2d9b3eac02c3e997de
zengchuixin <zengchuixin@126.com>
2019-08-08 15:11:22 +0800

    Windows | 获取本地WIFI配置文件信息

---
+++ Windows/Get-WIFIProfileInfo.ps1
@@ -0,0 +1,17 @@
+﻿# 获取WIFI的配置信息（名称、密码等）
+
+$output = netsh.exe wlan show profiles name="test AP" key=clear
+
+$ssidsearchredult = $output | Select-String -Pattern 'SSID Name'
+
+$profilename = ($ssidsearchredult -split ":")[-1].Trim() -replace '"'
+
+$pwsearchresult = $output | Select-String -Pattern 'Key Content'
+$pw = {$pwsearchresult -split ":"}[-1].Trim() -replace '"'
+
+[PSCustomObject]@{
+    wifiprofilename = $profilename
+    password = $pw
+}
+
+# https://devblogs.microsoft.com/scripting/get-wireless-network-ssid-and-password-with-powershell/
