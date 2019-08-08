182479622ae97de8e1a65f7e5da8c18b1bca9234
zengchuixin <zengchuixin@126.com>
2019-08-08 15:11:54 +0800

    windows | 批量删除本地某一种类型的文件

---
+++ Windows/Remove-MutiChildItem.ps1
@@ -0,0 +1,4 @@
+﻿Get-ChildItem C:\LogDir\*.LOG | Where-Object { $_.LastWriteTime.adddays(7) -lt $Today } | Remove-Item -whatif
+
+# 设置计划任务程序
+PowerShell.exe -executionpolicy Bypass -file .\Cleanup.ps1
